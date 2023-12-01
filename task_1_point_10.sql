ef78c5b938e82f7290f90db4f83a0bfd11334be2
Pavel <151017209+AlexandrBelinskiy@users.noreply.github.com>
2023-12-01 05:29:49 +0300

    Update task_1_point_10.sql

--- task_1_point_10.sql
+++ task_1_point_10.sql
@@ -2,5 +2,5 @@
 	10. Показать поле Name из таблицы Person.CountryRegion.
 	Заменить название региона (Name) на NULL, если регион называется American Samoa. 
 */
-SELECT COALESCE(NULLIF("Name", 'American Samoa'), NULL) AS "Name" 
+SELECT NULLIF("Name", 'American Samoa') AS "Name" 
 	FROM "Person"."CountryRegion";
